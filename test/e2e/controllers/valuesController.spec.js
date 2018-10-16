85afd780b532732ae535ca9bbfc0a7ea96b49c85
farooqam <farooq_am@hotmail.com>
2018-10-15 23:07:31 -0700

    E2E test refactor.

--- test/e2e/controllers/valuesController.spec.js
+++ test/e2e/controllers/valuesController.spec.js
@@ -6,7 +6,7 @@ const serverFactory = require("../serverFactory");
 var client;
 var server;
 
-describe("GET", () =>{
+describe("Values Controller E2E Tests", () => {
     before((done) => {
         client = clientFactory.createClient();
         server = serverFactory.createServer();
@@ -18,97 +18,67 @@ describe("GET", () =>{
         server.close(done);
     });
 
-    it("should return the expected response", (done) => {
-        const expectedValues = [1, 2, 3];
+    describe("GET", () => {
+        it("should return the expected response", (done) => {
+            const expectedValues = [1, 2, 3];
 
-        client.get(
-            "/api/values",
-            (err, _, res, actualValues) => {
-                expect(err).to.be.null;
-                expect(res.statusCode).to.equal(200);
-                expect(actualValues).to.have.all.members(expectedValues);
-            });
+            client.get(
+                "/api/values",
+                (err, _, res, actualValues) => {
+                    expect(err).to.be.null;
+                    expect(res.statusCode).to.equal(200);
+                    expect(actualValues).to.have.all.members(expectedValues);
+                });
 
-        done();
-    });
-}
-);
+            done();
+        });
+    }
+    );
 
-describe("POST", () =>{
-    before((done) => {
-        client = clientFactory.createClient();
-        server = serverFactory.createServer();
-        done();
-    });
+    describe("POST", () =>{
+        it("should return the expected response", (done) => {
+            const expectedValues = [1, 2, 3];
+            client.post(
 
-    after((done) => {
-        client.close();
-        server.close(done);
-    });
+                "/api/values",
+                expectedValues,
+                (err, _, res, actualValues) => {
+                    expect(err).to.be.null;
+                    expect(res.statusCode).to.equal(201);
+                    expect(actualValues).to.have.all.members(expectedValues);
+                    done();
+                });
+        });
+    }
+    );
 
-    it("should return the expected response", (done) => {
-        const expectedValues = [1, 2, 3];
-        client.post(
+    describe("PUT", () =>{
+        it("should return the expected response", (done) => {
+            client.put(
+                "/api/values",
+                [1, 2, 3],
+                (err, _, res, resBody) => {
+                    expect(err).to.be.null;
+                    expect(resBody).to.be.empty;
+                    expect(res.statusCode).to.equal(204);
+                    done();
+                });
+        });
+    }
+    );
 
-            "/api/values",
-            expectedValues,
-            (err, _, res, actualValues) => {
-                expect(err).to.be.null;
-                expect(res.statusCode).to.equal(201);
-                expect(actualValues).to.have.all.members(expectedValues);
-                done();
-            });
-    });
-}
-);
+    describe("DELETE", () =>{
+        it("should return the expected response", (done) => {
+            client.del(
+                "/api/values",
+                (err, _, res, resBody) => {
+                    expect(err).to.be.null;
+                    expect(resBody).to.be.empty;
+                    expect(res.statusCode).to.equal(204);
+                    done();
+                });
+        });
+    }
+    );
+});
 
-describe("PUT", () =>{
-    before((done) => {
-        client = clientFactory.createClient();
-        server = serverFactory.createServer();
-        done();
-    });
-
-    after((done) => {
-        client.close();
-        server.close(done);
-    });
-
-    it("should return the expected response", (done) => {
-        client.put(
-            "/api/values",
-            [1, 2, 3],
-            (err, _, res, resBody) => {
-                expect(err).to.be.null;
-                expect(resBody).to.be.empty;
-                expect(res.statusCode).to.equal(204);
-                done();
-            });
-    });
-}
-);
-
-describe("DELETE", () =>{
-    before((done) => {
-        client = clientFactory.createClient();
-        server = serverFactory.createServer();
-        done();
-    });
-
-    after((done) => {
-        client.close();
-        server.close(done);
-    });
-
-    it("should return the expected response", (done) => {
-        client.del(
-            "/api/values",
-            (err, _, res, resBody) => {
-                expect(err).to.be.null;
-                expect(resBody).to.be.empty;
-                expect(res.statusCode).to.equal(204);
-                done();
-            });
-    });
-}
-);
